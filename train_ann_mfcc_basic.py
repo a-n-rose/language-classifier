47b771e7ffbfb7363ff88b5ada5a25d7f150caae
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-08-14 14:34:56 +0200

    added input() for user to check global variables

--- train_ann_mfcc_basic.py
+++ train_ann_mfcc_basic.py
@@ -15,6 +15,9 @@ from pympler import tracker
 #import dataset
 database = 'sp_mfcc.db'
 table = 'mfcc_40'
+batchsize = 10
+epochs = 10
+modelname = 'mfcc_matched_eng_germ'#just an example
 
 
 if __name__ == '__main__':
@@ -66,81 +69,83 @@ if __name__ == '__main__':
         conn = sqlite3.connect(database)
         c = conn.cursor()
         
-        
-        
-        
-        
-        #import train (1), validate(2) and test (3) data sets 
-        c.execute("SELECT * FROM {} WHERE dataset='{}'".format(table,1))
-        dataset_train = c.fetchall()
-        df_train = pd.DataFrame(dataset_train)
-        
-        #c.execute("SELECT * FROM {} WHERE dataset='{}'".format(table,2))
-        #dataset_validate = c.fetchall()
-        #df_validate = pd.DataFrame(dataset_validate)
-        
-        c.execute("SELECT * FROM {} WHERE dataset='{}'".format(table,3))
-        dataset_test = c.fetchall()
-        df_test = pd.DataFrame(dataset_test)
-        
-        #should apply limits/ensure a healthy balance
-        X_train = df_train.iloc[:,:40].values
-        y_train = df_train.iloc[:,-1].values
-        
-        #X_val = df_validate.iloc[:,:40].values
-        #y_val = df_validate.iloc[:,-1].values
-        
-        X_test = df_test.iloc[:,:40].values
-        y_test = df_test.iloc[:,-1].values
+        check_variables = input("\nIMPORTANT!!!!\nHave you checked the global variables at the top of this script? Especially how the model should be saved? It would be a pity to save over a previous model... (Y or N): ")
+        if 'y' in check_variables.lower():
+
+            #import train (1), validate(2) and test (3) data sets 
+            c.execute("SELECT * FROM {} WHERE dataset='{}'".format(table,1))
+            dataset_train = c.fetchall()
+            df_train = pd.DataFrame(dataset_train)
+            
+            #c.execute("SELECT * FROM {} WHERE dataset='{}'".format(table,2))
+            #dataset_validate = c.fetchall()
+            #df_validate = pd.DataFrame(dataset_validate)
+            
+            c.execute("SELECT * FROM {} WHERE dataset='{}'".format(table,3))
+            dataset_test = c.fetchall()
+            df_test = pd.DataFrame(dataset_test)
+            
+            #should apply limits/ensure a healthy balance
+            X_train = df_train.iloc[:,:40].values
+            y_train = df_train.iloc[:,-1].values
+            
+            #X_val = df_validate.iloc[:,:40].values
+            #y_val = df_validate.iloc[:,-1].values
+            
+            X_test = df_test.iloc[:,:40].values
+            y_test = df_test.iloc[:,-1].values
 
 
-        from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-        labelencoder_y = LabelEncoder()
-        y_train = labelencoder_y.fit_transform(y_train)
-        #y_val =labelencoder_y.fit_transform(y_val)
-        y_test = labelencoder_y.fit_transform(y_test)
+            from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+            labelencoder_y = LabelEncoder()
+            y_train = labelencoder_y.fit_transform(y_train)
+            #y_val =labelencoder_y.fit_transform(y_val)
+            y_test = labelencoder_y.fit_transform(y_test)
 
-        
-        #feature scaling
-        from sklearn.preprocessing import StandardScaler
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        #X_val = sc.transform(X_val)
+            
+            #feature scaling
+            from sklearn.preprocessing import StandardScaler
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            #X_val = sc.transform(X_val)
 
-        #ANN
-        import keras
-        from keras.models import Sequential
-        from keras.layers import Dense
-        
-        classifier = Sequential()
-        classifier.add(Dense(activation = 'relu',units=20,input_dim=X_train.shape[1],kernel_initializer='uniform'))
-        
-        classifier.add(Dense(activation='relu',units=20,kernel_initializer='uniform'))
-        
-        classifier.add(Dense(activation='sigmoid',units=1,kernel_initializer='uniform'))
-        
-        classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
-        
-        #numbers: batchsize and epochs
-        classifier.fit(X_train,y_train,1,1)
-        
-        y_pred = classifier.predict(X_test)
-        y_pred = (y_pred > 0.5)
-        
-        from sklearn.metrics import confusion_matrix
-        y_test = y_test.astype(bool)
-        cm = confusion_matrix(y_test,y_pred)
-        score = classifier.evaluate(X_train,y_train,verbose=0)
-        acc = "%s: %.2f%%" % (classifier.metrics_names[1], score[1]*100)
-        print(acc)
-        
-        model_json = classifier.to_json()
-        modelname = 'mfcc_traintest'
-        with open(modelname+'.json','w') as json_file:
-            json_file.write(model_json)
-        classifier.save_weights(modelname+'.h5')
-        print('Done!')
+            #ANN
+            import keras
+            from keras.models import Sequential
+            from keras.layers import Dense
+            
+            classifier = Sequential()
+            classifier.add(Dense(activation = 'relu',units=20,input_dim=X_train.shape[1],kernel_initializer='uniform'))
+            
+            classifier.add(Dense(activation='relu',units=20,kernel_initializer='uniform'))
+            
+            classifier.add(Dense(activation='sigmoid',units=1,kernel_initializer='uniform'))
+            
+            classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+            
+            #numbers: batchsize and epochs
+            classifier.fit(X_train,y_train,batchsize,epochs)
+            
+            y_pred = classifier.predict(X_test)
+            y_pred = (y_pred > 0.5)
+            
+            from sklearn.metrics import confusion_matrix
+            y_test = y_test.astype(bool)
+            cm = confusion_matrix(y_test,y_pred)
+            score = classifier.evaluate(X_train,y_train,verbose=0)
+            acc = "%s: %.2f%%" % (classifier.metrics_names[1], score[1]*100)
+            print(acc)
+            
+            model_json = classifier.to_json()
+            with open(modelname+'.json','w') as json_file:
+                json_file.write(model_json)
+            classifier.save_weights(modelname+'.h5')
+            print('Done!')
+        else:
+            print_message = "\nRun the script after you check the global variables."
+            print(print_message)
+            logging.info(print_message)
     except sqlite3.Error as e:
         logging.exception("Database error: %s" % e)
     except Exception as e:

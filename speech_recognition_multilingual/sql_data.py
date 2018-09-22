593949340c41ee28293550042bafc0b88e33f30a
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-09-22 22:18:48 +0200

    added functionality for no specifications at all (even column and variables)

--- speech_recognition_multilingual/sql_data.py
+++ speech_recognition_multilingual/sql_data.py
@@ -11,20 +11,30 @@ class LimitMissingError(Error):
    pass
 
 
-def sqldata2df(c,tablename,column_value_list,limit=None,row_start=None):
+def sqldata2df(c,tablename,column_value_list=None,limit=None,row_start=None):
     try:
         col_val = []
-        for item in column_value_list:
-            item[1] = "'{}'".format(item[1])
-            col_val.append('='.join(map(str, item)))
+        if column_value_list:
+            extra = " WHERE"
+            for item in column_value_list:
+                item[1] = "'{}'".format(item[1])
+                col_val.append('='.join(map(str, item)))
+        else:
+            extra = ""
         if limit and row_start:
-            col_val[-1]+=(" LIMIT %s OFFSET %s" % (limit,row_start))
+            if len(col_val) > 0:
+                col_val[-1]+=(" LIMIT %s OFFSET %s" % (limit,row_start))
+            else:
+                col_val = [" LIMIT %s OFFSET %s" % (limit,row_start)]
         elif limit:
-            col_val[-1]+=(" LIMIT %s" % (limit))
+            if len(col_val) > 0:
+                col_val[-1]+=(" LIMIT %s" % (limit))
+            else:
+                col_val.append(" LIMIT %s" % (limit))
         elif row_start:
             raise LimitMissingError
             
-        msg = ''' SELECT * FROM {} WHERE %s'''.format(tablename) % (" AND ".join(col_val))
+        msg = ''' SELECT * FROM {}{} %s'''.format(tablename,extra) % (" AND ".join(col_val))
         print(msg)
         c.execute(msg)
         data = c.fetchall()
@@ -32,7 +42,3 @@ def sqldata2df(c,tablename,column_value_list,limit=None,row_start=None):
         return df
     except LimitMissingError:
         print("\nLimitMissingError: Need a LIMIT value in order to specify a ROWSTART value.\n")
-
-
-
-

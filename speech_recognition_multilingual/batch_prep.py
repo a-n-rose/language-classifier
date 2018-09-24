578b9fdbacb2dd5653e5e33e12240cce375f85d8
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-09-24 20:33:15 +0200

    Turned IPA --> dict w/ ints as values; then added the ints to the last three columns of the mfcc data
    This keeps the labels with the corresponding data; possible to then separate into X and y, and then to one-hot-encode the y values before the lstm

--- speech_recognition_multilingual/batch_prep.py
+++ speech_recognition_multilingual/batch_prep.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import config
 from pathlib import Path
+import time
 
 
 class Batch_Data:
@@ -25,8 +26,43 @@ class Batch_Data:
         tgz_name = "{}.tgz_{}".format(path_split,wav)
         return tgz_name
 
+    def build_ipa_dict(self,ipa_list):
+        dict_ipa = dict()
+        for num in range(len(ipa_list)):
+            dict_ipa[ipa_list[num]] = num
+        print(dict_ipa)
+        self.dict_ipa = dict_ipa
+        return self
+
+    def all_ipa_present(self):
+        try:
+            start = time.time()
+            ipa_chars = []
+            count = 0
+            for annotation in self.ipa[:,3]:
+                for char in annotation:
+                    count += 1
+                    if char in ipa_chars:
+                        pass
+                    else:
+                        ipa_chars.append(char)
+            self.build_ipa_dict(ipa_chars)
+            end = time.time()
+            total_time = end - start
+            return ipa_chars, total_time, count
+        except Exception as e:
+            print(e)
+            
+    def retrieve_ipa_key(self,ipa_list):
+        ipa_keys = []
+        for char in ipa_list:
+            ipa_keys.append(self.dict_ipa[char])
+        return ipa_keys
+
     #for each row in data_ipa
     def generate_batch(self,batch_size,ipa_window,ipa_shift):
+        if ipa_shift > ipa_window:
+            raise ShiftLargerThanWindowError("The shift cannot exceed the size of the window of IPA characters.")
         if self.data_index is not None:
             data_index = self.data_index
         else:
@@ -58,22 +94,34 @@ class Batch_Data:
         
         
         #figure out how many batches of MFCC data I have for the total number of IPA chars
-        #do I want to overlap? Yes I think so.. 
-        
-        #num batches = num_ipa - 2
-        total_batches = num_ipa - (ipa_window - 1)
+        #do I want to overlap? Not right now..
+        overlap = False
+        #make sure there is a total of 3 IPA characters (or the size of the window) per input (don't end up with with only 1 IPA character as classification sequence)
+        if overlap == False:   
+            num_ipa_diff = num_ipa % ipa_window
+            num_ipa -= num_ipa_diff
+            annotation_ipa = annotation_ipa[:num_ipa]
+            
+        total_batches = int(num_ipa/ipa_shift - (ipa_window - ipa_shift))
         #create skeleton for where batches will be collected
-        batch = np.zeros(shape=(total_batches,batch_size,num_features))
+        batch = np.zeros(shape=(total_batches,batch_size,num_features+ipa_window))
         
         for batch_iter in range(total_batches):
             start = batch_iter * (num_mfcc_per_ipa * ipa_shift) #shifting at indicated shift length (e.g. if ipa_shift = 1, then shift 1 letter at a time)
             end = start + batch_mfcc #window of 3 letters
             assert end < len(mfcc) 
-            batch[batch_iter]=mfcc[start:end,:]
+            index_ipa = batch_iter * ipa_shift
+            ipa_label = annotation_ipa[index_ipa:index_ipa+ipa_window]
+            ipa_ints = self.retrieve_ipa_key(ipa_label)
+            batch_input = mfcc[start:end,:]
+            len_mfccs = len(batch_input)
+            add_ints = np.repeat([ipa_ints],len_mfccs,axis=0)
+            batch_input = np.c_[batch_input,add_ints]
+            batch[batch_iter]=batch_input
         
         if data_index < len(ipa):
             self.data_index += 1
         else:
             self.data_index = None
             print("Through all of IPA data")
-        return batch, annotation_ipa
+        return batch, total_batches
--- speech_recognition_multilingual/batch_train.py
+++ speech_recognition_multilingual/batch_train.py
@@ -12,32 +12,23 @@ database_ipa = 'speech2IPA3.db'
 table_ipa = 'speech2ipa'
 ipa = Connect_db(database_ipa,table_ipa)
 
-
 database_mfcc = 'sp_mfcc_IPA3.db'
 table_mfcc = 'mfcc_40'
 mfcc = Connect_db(database_mfcc, table_mfcc)
 
-
 data_ipa = ipa.sqldata2df(limit=1000000)
 data_mfcc = mfcc.sqldata2df(limit=1000000)
 
-
 x_ipa = data_ipa.values
 x_mfcc = data_mfcc.values
-#print(x_mfcc[:,40])
 
 batch_prep = Batch_Data(x_ipa,x_mfcc)
-batch1 = batch_prep.generate_batch(18,3,1)
+ipa_list = batch_prep.all_ipa_present()
 
-print(batch1[1])
+batch_mfcc,total_batches = batch_prep.generate_batch(batch_size=18,ipa_window=3,ipa_shift=3)
 
-
-'''
-Got this working, but need to figure out how to label the batches... how to connect them to the ipa characters
-
-Next step: one-hot-encode the ipa characters?
-
-Also, had a problem if batch_size was 20 (vs. 18) Perhaps different way of adding data to the numpy array? So there are zeros where there aren't any data added?
-
-Note: take length of batch and associate that with the length of ipa stuff
-'''
+for i in range(total_batches):
+    print("Batch number: {}\n\n".format(i+1))
+    len_batches = len(batch_mfcc[i])
+    for j in range(len_batches):
+        print("IPA values are: {}".format(batch_mfcc[i][j][40:]))

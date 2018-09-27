a3594ffcf41c0aceb9da534974fefadf98dc8f4c
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-09-27 22:37:01 +0200

    bug fix: nameError: model_name vs modelname
    Also cleaned up code

--- english_speech_to_ipa/batch2model.py
+++ english_speech_to_ipa/batch2model.py
@@ -44,6 +44,7 @@ if __name__=="__main__":
 
     logging.info("Database where data is pulled from: {}".format(database))
     logging.info("Table where data is pulled from: {}".format(table_final))
+    
 
     try: 
         #The following commands allow me to know how many classes of IPA combinations I have and such.
@@ -60,18 +61,16 @@ if __name__=="__main__":
         #get IPA values given ipa window and shift (how many classes I have)        
         ipa_window = 3
         window_shift = 3
-        ipa_list,num_classes,num_classes_total = bp.doc_ipa_present(ipa_window,window_shift)
+        ipa_list,num_classes_local,num_classes_total = bp.doc_ipa_present(ipa_window,window_shift)
         #define batches... just to be sure
         batch_size = 20
         bp.def_batch(batch_size)
-        logging.info("\n\nIPA characters existent in dataset: \n{}\n\n".format(ipa_list))
-        logging.info("Number of local classes: {}".format(num_classes))
-        logging.info("Number of total possible classes: {}".format(num_classes_total))
-        print("Number of local classes: {}".format(num_classes))
-        print("Number of total possible classes: {}".format(num_classes_total))
-        #print(bp.classes)
+        logging.info("Number of classes in dataset: {}".format(num_classes_local))
+        logging.info("Number of total possible classes (various combinations of IPA characters in sets of 3): {}".format(num_classes_total))
+
         
-        #get train data:
+        ############ get data: ############
+        
         df_train = db.sqldata2df(table_final,column_value_list=[['dataset',train_label]])
         df_val = db.sqldata2df(table_final,column_value_list=[['dataset',val_label]])
         df_test = db.sqldata2df(table_final,column_value_list=[['dataset',test_label]])
@@ -84,21 +83,20 @@ if __name__=="__main__":
         x_y_val = bp.get_x_y(df_val)
         x_y_test = bp.get_x_y(df_test)
         
-        #prep data for LSTM
+        
+        ############ prep data for LSTM ############
         
         #first normalize data    
         X_train = bp.normalize_data(x_y_train[0])
         X_val = bp.normalize_data(x_y_val[0])
         X_test = bp.normalize_data(x_y_test[0])
-    
-    
+        
         #feature scaling
         sc = StandardScaler()
         X_train = sc.fit_transform(X_train)
         X_val = sc.transform(X_val)
         X_test = sc.transform(X_test)
 
-            
         #For LSTM, make 3d (numrows,batch_size(i.e. num of sequences bp.batch_size),num_features)
         X_train = bp.make2d_3d(x_y_train[0])
         y_train = bp.make2d_3d(x_y_train[1])
@@ -106,69 +104,39 @@ if __name__=="__main__":
         y_val = bp.make2d_3d(x_y_val[1])
         X_test = bp.make2d_3d(x_y_test[0])
         y_test = bp.make2d_3d(x_y_test[1])
+        
 
+        ############ Build the model ############
+        
+        
+        #Apply the data in batches (of 20)
+        train_data_generator = KerasBatchGenerator(X_train,y_train,num_steps=bp.batch_size,batch_size_model=bp.batch_size,num_features=bp.num_features,num_output_labels=y_train.shape[2],skip_step=1)
+        
+        val_data_generator = KerasBatchGenerator(X_val,y_val,num_steps=bp.batch_size,batch_size_model=bp.batch_size,num_features=bp.num_features,num_output_labels=y_train.shape[2],skip_step=1)
+        
 
-        #categorical data for multiple classes
-        #num classes based on number of possible 3-letter combinations of all ipa characters
-        num_classes = num_classes
-        
-        #do I need to first see which classes are in the y data?
-        y_train_num_classes = bp.classes_present(y_train)
-        y_val_num_classes = bp.classes_present(y_val)
-        y_test_num_classes = bp.classes_present(y_test)
-        
-        #print("Num Classes in \n~ train data: {} \n~ validation data: {} \n~ test data: {}".format(y_train_num_classes,y_val_num_classes,y_test_num_classes))
-        
-        #print("Shape of input data: {}".format(X_train.shape))
-        #trainx20 = X_train[0:20]
-        #print("Just 20 rows of input: {}".format(trainx20))
-        #print("Size of that: {}".format(trainx20.shape))
-        #print("Shape of output data: {}".format(y_train.shape))
-        #trainy20 = y_train[0:20]
-        #print("Just 20 rows of output: {}".format(trainy20))
-        #print("Size of that: {}".format(trainy20.shape))
-        
-        input_dim = X_train.shape[2]
-        vector = y_train.shape[0]
-        #Build Model:
-        #raise SystemExit
-        
-        batch_size_model = 5
-        
-        train_data_generator = KerasBatchGenerator(X_train,y_train,num_steps=bp.batch_size,batch_size_model=batch_size_model,num_features=bp.num_features,classes_total=bp.num_classes_total,num_output_labels=y_train.shape[2],skip_step=1)
-        
-        
-        val_data_generator = KerasBatchGenerator(X_val,y_val,num_steps=bp.batch_size,batch_size_model=batch_size_model,num_features=bp.num_features,classes_total=bp.num_classes_total,num_output_labels=y_train.shape[2],skip_step=1)
-        
-        
-        
-        #hidden layer: 40 * 2
+        #hidden layer: 40 features * 2 (I saw something like this somewhere..)
         model = Sequential()
-        #model.add(Embedding(bp.num_classes_total,bp.num_features,input_length=bp.batch_size))
+
         model.add(LSTM(80,return_sequences=True,input_shape=(bp.batch_size,bp.num_features)))
         model.add(Dropout(0.2))
         
         model.add(LSTM(80,return_sequences=True))
         model.add(Dropout(0.2))
         
-        #model.add(Flatten())
         model.add(TimeDistributed(Dense(bp.num_classes_total)))
         model.add(Activation('softmax'))
         
-    
-        #in order to avoid memory error problem when assigning one-hot-encoded values
+        # when not assigning one-hot-encoded values to y_train multi-class data, use 'sparse_categorical_crossentropy' (as long as labels are integers)
         model.compile(loss='sparse_categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
         
-        #model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
-        
-        #batchsize: 80 * 2
-        #numbers: batchsize and epochs
-        #model.fit(X_train,y_train,epochs=50,batch_size=160,validation_data=(X_val,y_val))
         epochs = 5
         ##for when enough data is available (I think validation data is not sufficient)
         #model.fit_generator(train_data_generator.generate(),len(X_train)//(train_data_generator.batch_size_model*train_data_generator.skip_step),epochs, validation_data=val_data_generator.generate(),validation_steps=len(X_val)//(val_data_generator.batch_size_model*val_data_generator.skip_step))
         model.fit_generator(train_data_generator.generate(),len(X_train)//(train_data_generator.batch_size_model*train_data_generator.skip_step),epochs)
         
+        
+        ############ Evaluate the model ############
         
         score = model.evaluate(X_test,y_test,verbose=0)
         acc = "%s: %.2f%%" % (model.metrics_names[1], score[1]*100)
@@ -176,16 +144,25 @@ if __name__=="__main__":
         print(acc)
         logging.info("Model Accuracy: {}".format(acc))
         
-        print('Saving Model')
+        
+        ############ Save the model ############
+        
         date = get_date()
         model_name = 'engspeech2ipa_{}'.format(date)
+        logging.info('Saving Model as {}'.format(model_name))
         model_json = model.to_json()
-        with open(modelname+'.json','w') as json_file:
+        with open(model_name+'.json','w') as json_file:
             json_file.write(model_json)
-        model.save_weights(modelname+'.h5')
-        print('Done!')
+        model.save_weights(model_name+'.h5')
+        logging.info("Model saved!")
+        
+        
+        ############ Close up shop ############
+        
         elapsed_time_hours = (time.time()-prog_start)/3600
         timepassed_message = 'Elapsed time in hours: {}'.format(elapsed_time_hours)
+        logging.info(timepassed_message)
+        print(timepassed_message)
         
 
     except DatabaseLimitError as e:

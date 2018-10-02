f953fcdaf70e4225d86d8573c9bdb6b63d29ee03
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-10-02 10:29:09 +0200

    included varify variables; made naming of tables dependent on variable input; made ipa_shift depend on its relation to ipa_window; changed name of 'ipa_chars_only' to 'ipa_stress'
    increase reliability of data and clarity of variable names

--- english_speech_to_ipa/combine_align_ipa_mfcc_data.py
+++ english_speech_to_ipa/combine_align_ipa_mfcc_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sqlite3 import Error
 
 from sql_data import Connect_db
-from batch_prep2 import Batch_Data
+from batch_prep import Batch_Data
 
 from Errors import Error, DatabaseLimitError, ValidateDataRequiresTestDataError, ShiftLargerThanWindowError, TrainDataMustBeSetError, EmptyDataSetError, MFCCdataNotFoundError
 
@@ -15,9 +15,18 @@ import time
 import logging.handlers
 from my_logger import start_logging, get_date
 #for logging:
-script_purpose = 'speechwnoise_ipa_mfcc_combine_data' #will name logfile 
+script_purpose = 'speech_align_ipa_mfcc_data' #will name logfile 
 current_filename = os.path.basename(__file__)
 session_name = get_date() #make sure this session has a unique identifier - link to model name and logging information
+
+language= 'english'
+noise_doc = True # False
+num_mfcc = 13 # 40
+ipa_window = 3
+ipa_shift = ipa_window #(if equal to ipa_window, no overlap)
+overlap = ipa_shift < ipa_window
+ipa_stress = True # False (if stress characters should be included or not)
+sequence_size = 20
 
 
 if __name__=="__main__":
@@ -25,11 +34,11 @@ if __name__=="__main__":
     prog_start = time.time()
     logging.info(prog_start)
     
-    database = 'speech_wnoise_ipa_mfcc.db'
+    database='speech_noise_{}_ipa_{}mfcc.db'.format(noise_doc, num_mfcc)
     table_ipa = 'speech_as_ipa'
     table_mfcc = 'speech_as_mfcc'
     #table where combined datasets will be saved
-    table_final = 'english_40mfcc_ipawindow3_ipashift3_1label_datasets20batches_idclasses_ipacharsonly'
+    table_final = '{}_{}mfcc_noise{}_ipawindow{}_ipashift{}_overlap{}_ipastress{}_datasets{}batches'.format(language,num_mfcc,noise_doc,ipa_window,ipa_shift,overlap,ipa_stress,sequence_size)
     db = Connect_db(database,table_ipa,table_mfcc,table_final)
 
     
@@ -38,6 +47,11 @@ if __name__=="__main__":
     logging.info("Data saved in table: {}".format(table_final))
 
     try:
+        print("Are these variables correct?\nDatabase to pull data: {}\nTable IPA: {}\nTable MFCC: {}\nName for the new output table: {}".format(database,table_ipa,table_mfcc,table_final))
+        check_var = input()
+        if 'y' not in check_var.lower():
+            raise SystemExit("Please correct the variables and rerun the script.")
+        
         data_ipa = db.sqldata2df(db.table_ipa,limit=1000000)
         
         logging.info("Loaded data from table {}".format(table_ipa))
@@ -48,7 +62,7 @@ if __name__=="__main__":
         x_mfcc = data_mfcc.values
 
         bp = Batch_Data(x_ipa,x_mfcc)
-        ipa_list, num_classes, num_classes_total = bp.doc_ipa_present(ipa_window=3,ipa_shift=3,ipa_chars_only=True)
+        ipa_list, num_classes, num_classes_total = bp.doc_ipa_present(ipa_window=ipa_window,ipa_shift=ipa_shift,ipa_stress=ipa_stress)
         #logging.info("\n\nIPA characters existent in dataset: \n{}\n\n".format(ipa_list))
         #logging.info("Number of total classes: {}".format(num_classes))
         print("Number of local classes: {}".format(num_classes))
@@ -66,7 +80,7 @@ if __name__=="__main__":
         ipa_datasets = bp.get_datasets()
         #define perameters for the batches - will be defined to all batches made
         #in this class instance
-        bp.def_batch(batch_size=20)
+        bp.def_batch(batch_size=sequence_size)
         #save the batches to sql database
         count = 0
         for batch_item in ipa_datasets:
@@ -110,18 +124,9 @@ if __name__=="__main__":
         logging.error("MFCCdataNotFoundError: {}".format(e))
     except Error as e:
         logging.error("Database error: {}".format(e))
-    except SystemExit:
-        logging.error("SystemExit: Had to exit program early.")
+    except SystemExit as e:
+        logging.error("SystemExit: {}".format(e))
     #Close database connections:
     finally:
         db.close_conn()
         logging.info("database {} successfully closed.".format(database))
-
-
-'''
-ToDo: 
-1) improve status bar
-* https://stackoverflow.com/questions/3002085/python-to-print-out-status-bar-and-percentage
-
-Question I have: would it make a difference if ipa stress markers were included? 
-'''
